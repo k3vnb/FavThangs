90606978a4d7dd8c7e559e8459eda91a04995a6c
Ginger Lee Kretschmer and Kevin Boyle <kevin@>
2017-10-23 11:05:00 -0700

    add javascript array to take form answers and show them after submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 $(function() {
-  $("form.survey").submit(function(event) {
-    alert("Nice!");
-  })
+  $(".jumbotron a").click(function() {
+    $(".survey").show();
+  });
 
+  $(".survey form").submit(function(event) {
+    event.preventDefault();
+
+    var firstName = $("input#first").val();
+    var lastName = $("input#last").val();
+    var answers = ["q1", "q2", "q3", "q4"];
+
+    answers.forEach(function(answer) {
+      var userInput = $("input#" + answer).val();
+      $("." + answer).text(userInput);
+    });
+
+
+    $(".survey").hide();
+    $(".reveal").show();
+    $(".reveal #firstname").text(firstName).show();
+    $(".reveal #lastname").text(lastName).show();
+
+    if (answers === "") {
+      alert("Please complete form");
+      $("input#").addClass("is-invalid");
+    };
+  });
 });
